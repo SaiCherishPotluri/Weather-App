149a335ab3375a7b811d044c8879b725a45a61a5
Sai Cherish Potluri <cherishpotluri@sais-air.3500.dhcp.asu.edu>
2022-07-22 13:49:53 -0700

    create weatherapp

---
+++ src/components/api.js
@@ -0,0 +1,13 @@
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '3878567b05mshe9cdc8e6fa47d05p10d918jsnd6915669f353',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "14d761a2be2d3493e3cdfe2a7b82bb9c";
